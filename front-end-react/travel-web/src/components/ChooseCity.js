ca3ee04535ae67024ad7020a651f1bcbe0fab8c6
Brokl <brokl0802@yahoo.com.tw>
2019-07-09 20:50:39 -0400

    add CityList component

--- front-end-react/travel-web/src/components/ChooseCity.js
+++ front-end-react/travel-web/src/components/ChooseCity.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import {LocateCity} from './GoogleMapCity'
+import {CityList} from "./CityList"
 import { Link, withRouter  } from 'react-router-dom'
 
 let city={lat:47.608013, lng:-122.335167}
 
 class City extends React.Component{
-    state={path:[], latlng:{lat:0, lng:0}}
+    state={
+        path: [],
+        latlng: {lat: 51.506155, lng: -0.127824},
+        page: 0,
+        name: "London"
+    }
+
+    getPage = (curPage, cityName, cityLatLng) => {
+        this.setState({page: curPage});
+        this.setState({name: cityName});
+        this.setState({latlng: cityLatLng});
+    }
 
     zoomToCity = ()=>{
         this.setState({latlng: city})
@@ -18,7 +30,7 @@ class City extends React.Component{
     render(){
         return(
             <div>
-
+                <CityList getPage={this.getPage}/>
                 <button onClick={this.zoomToCity}>Locate City</button>
                 <LocateCity latlng={this.state.latlng}/>
                 {/*<NavLink to="/plan">Contact</NavLink>*/}

f5d7d1cd2fef03bbdbd6745dac25e5832f30d572
Jieyu You <jyou61@gatech.edu>
2019-07-16 03:00:38 -0500

    .

--- front-end-react/travel-web/src/components/GoogleMapTest.js
+++ front-end-react/travel-web/src/components/GoogleMapTest.js
@@ -1,73 +1,74 @@
 import * as React from 'react';
 import {API_KEY} from "../constants.js"
+// googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`,
+
 const { compose, withProps, withStateHandlers } = require("recompose");
 const {
     withScriptjs,
     withGoogleMap,
     GoogleMap,
-    Marker,
-    InfoWindow,
+    OverlayView,
 } = require("react-google-maps");
-const { InfoBox } = require("react-google-maps/lib/components/addons/InfoBox");
 
+const getPixelPositionOffset = (width, height) => ({
+    x: -(width / 2),
+    y: -(height / 2),
+})
 
-class MyMap extends React.Component{
-    state = {
-        isOpen: false,
-    }
-
-    onToggleOpen=()=>{
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
-    }
-
-    render(){
-        return(
-            <GoogleMap
-                defaultZoom={5}
-                defaultCenter={this.props.center}
-            >
-                <InfoBox
-                    defaultPosition={new window.google.maps.LatLng(this.props.center.lat, this.props.center.lng)}
-                    options={{ closeBoxURL: ``, enableEventPropagation: true }}
-                >
-                    <div style={{ backgroundColor: `yellow`, opacity: 0.75, padding: `12px` }}>
-                        <div style={{ fontSize: `16px`, fontColor: `#08233B` }}>
-                            Hello, Taipei!
-                        </div>
-                    </div>
-                </InfoBox>
-                <Marker
-                    position={{ lat: 22.6273, lng: 120.3014 }}
-                    onClick={this.onToggleOpen}
-                >
-                    {this.state.isOpen && <InfoBox
-                        onCloseClick={this.onToggleOpen}
-                        options={{ closeBoxURL: ``, enableEventPropagation: true }}
-                    >
-                        <div style={{ backgroundColor: `yellow`, opacity: 0.75, padding: `12px` }}>
-                            <div style={{ fontSize: `16px`, fontColor: `#08233B` }}>
-                                Hello, Kaohsiung!
-                            </div>
-                        </div>
-                    </InfoBox>}
-                </Marker>
-            </GoogleMap>
-        )
-    }
-}
-
-
-export const Test =  compose(
+export const Test = compose(
     withProps({
         googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`,
-        loadingElement: <div style={{ height: `100%` }} />,
-        containerElement: <div style={{ height: `400px` }} />,
-        mapElement: <div style={{ height: `100%` }} />,
-        center: { lat: 25.03, lng: 121.6 },
+        loadingElement:<div style={{ height: `100%` }} />,
+        containerElement:<div style={{ height: `400px` }} />,
+        mapElement:<div style={{ height: `100%` }} />,
+        }
+    ),
+    withStateHandlers(() => ({
+        count: 0,
+    }), {
+        onClick: ({ count }) => () => ({
+            count: count + 1,
+        })
     }),
     withScriptjs,
     withGoogleMap
-)(MyMap
+)(props =>
+    <GoogleMap
+        defaultZoom={8}
+        defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    >
+        <OverlayView
+            position={{ lat: -34.397, lng: 150.644 }}
+            /*
+             * An alternative to specifying position is specifying bounds.
+             * bounds can either be an instance of google.maps.LatLngBounds
+             * or an object in the following format:
+             * bounds={{
+             *    ne: { lat: 62.400471, lng: -150.005608 },
+             *    sw: { lat: 62.281819, lng: -150.287132 }
+             * }}
+             */
+            /*
+             * 1. Specify the pane the OverlayView will be rendered to. For
+             *    mouse interactivity, use `OverlayView.OVERLAY_MOUSE_TARGET`.
+             *    Defaults to `OverlayView.OVERLAY_LAYER`.
+             */
+            mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+            /*
+             * 2. Tweak the OverlayView's pixel position. In this case, we're
+             *    centering the content.
+             */
+            getPixelPositionOffset={getPixelPositionOffset}
+            /*
+             * 3. Create OverlayView content using standard React components.
+             */
+        >
+            <div style={{ background: `white`, border: `1px solid #ccc`, padding: 15 }}>
+                <h1>OverlayView</h1>
+                <button onClick={props.onClick} style={{ height: 60 }}>
+                    I have been clicked {props.count} time{props.count > 1 ? `s` : ``}
+                </button>
+            </div>
+        </OverlayView>
+    </GoogleMap>
 );

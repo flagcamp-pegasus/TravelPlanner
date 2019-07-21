ab0aad21fe51a6785728f3bb45a9905ccd82f079
Jie Ma <ma333jie@berkeley.edu>
2019-07-21 16:36:04 -0700

    update Attraction display"

--- front-end-react/travel-web/src/components/AttractionPost.js
+++ front-end-react/travel-web/src/components/AttractionPost.js
@@ -4,25 +4,40 @@ import PropTypes from 'prop-types';
 import PubSub from 'pubsub-js'
 
 export class AttractionPost extends Component {
+
     static propTypes = {
         info: PropTypes.object.isRequired,
     }
+
 
     handleClick = ({location, name}) => {
         PubSub.publish('path', {location, name});
         console.log({location,name});
     }
+
+    handleURL = (photos) =>{
+        console.log(photos);
+        console.log(photos[0]);
+
+        return photos[0].getUrl({width: 300, height:300});
+
+    }
     render() {
         const { name, icon, photos, geometry, place_id, rating, vicinity,types} = this.props.info;
         const { location } = geometry;
         const { lat, lon } = location;
-        const photoUrl ="https://s3-media3.fl.yelpcdn.com/bphoto/EmBj4qlyQaGd9Q4oXEhEeQ/ms.jpg";
-        // const photourl = photos[0].getUrl();// set size of the url
+        const sampleURL = "https://s3-media3.fl.yelpcdn.com/bphoto/EmBj4qlyQaGd9Q4oXEhEeQ/ms.jpg";
+       // const url = this.handleURL(photos);
+
+
+       // console.log(photos);
+       // const photos1 = photos[0];//
+        //const photoUrl = photos[0].getUrl({width: 300, height: 300});// set size of the url
 
         return (
             <div>
                 <div className = 'place-name'>{name}</div>
-                <img alt="place image" src={photoUrl}/>
+                <img alt="place image" src={sampleURL}/>
                 <div className = 'place-info'>
                     <p className='place-rating'>rating : {rating}</p>
                     <p className="place-address">{vicinity}</p>

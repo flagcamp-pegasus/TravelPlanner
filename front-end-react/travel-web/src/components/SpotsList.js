80d8f02d27f59bb8030c22e305046596ce96f5de
luchen <44048738+luchen29@users.noreply.github.com>
2019-07-20 19:42:26 -0600

    Add files via upload

---
+++ front-end-react/travel-web/src/components/SpotsList.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import {mergeChildren} from './ListSort';
+import ListSort from './ListSort';
+import { Switch } from 'antd';
+import PubSub from 'pubsub-js';
+
+import '../styles/SpotsList.css';
+
+export class SpotsList extends React.Component {
+    state = {
+        path:[],
+        orderPath: [],
+    }
+    static propTypes = {
+        className: PropTypes.string,
+      };
+
+    static defaultProps = {
+        className: 'list-sort-demo',
+      };
+
+    startBtnActive = {
+        disabled: true,
+    };
+
+    transferMsg(msg) {
+        console.log(msg);
+      }
+
+    toggle = () => {
+        this.setState({
+          disabled: !this.startBtnActive.disabled,
+        });
+    };
+
+    componentDidMount() {
+        this.pubsub_token = PubSub.subscribe('path', (path, data) => {
+            this.setState({
+                path: this.state.path.concat(data)
+            })
+        })
+    }
+    componentWillUnmount() {
+        PubSub.unsubscribe(this.pubsub_token);
+    }
+
+    
+    render() {
+        const childrenToRender = this.state.path.map((item, index) => {
+            item.switchActive = false;
+            const {name} = item;
+            return (
+                <div key={index} className={`${this.props.className}-list`}>
+                    {name } 
+                    <Switch size="small" defaultunChecked/>
+                </div>
+            )
+        });
+        
+
+        return (
+            <div className={`${this.props.className}-div`}>
+                <div className={`${this.props.className}-wrapper`}>
+                    <ListSort 
+                        dragClassName="list-drag-selected"
+                        appearAnim = {{ animConfig: { marginTop: [5, 30], opacity: [1, 0] } }}>
+                        {childrenToRender}
+                    </ListSort>
+                    <button onClick={console.log(childrenToRender)}>click</button>
+                </div>
+            </div>
+        );
+    }
+}
+

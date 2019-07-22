0a62df569520601cfd13030d80a7b0d420ed021f
Jie Ma <ma333jie@berkeley.edu>
2019-07-22 16:28:04 -0700

    update attraction with innerHTML error"

--- front-end-react/travel-web/src/components/Attractions.js
+++ front-end-react/travel-web/src/components/Attractions.js
@@ -5,7 +5,7 @@ import { Tabs } from 'antd';
 import placesData from '../assets/data/GoogleSearchSampleData.json';
 import {AttractionList} from "./AttractionList";
 import {AttractionsSearch} from "./AttractionsSearch";
-import {LAT_SAMPLE, LON_SAMPLE,TYPE_FOOD} from "../constants";
+import {LAT_SAMPLE, LON_SAMPLE, TYPE_FOOD, TYPE_MUSEUM, TYPE_SHOPPING} from "../constants";
 import {API_FEE_KEY} from '../charge';
 import { Test } from './Test';
 
@@ -38,8 +38,10 @@ export class Attractions extends Component {
 
 
     attractionSearch = (type) => {
-       // console.log(type);
-      //  let service = new window.google.maps.places.PlacesService(document.getElementById(this.props.mapref));
+        // console.log(type);
+        debugger
+        let service = new window.google.maps.places.PlacesService(this.props.mapref);
+        debugger
         const { latlng } = this.props.city;
         console.log(this.props.city)
         let location = new window.google.maps.LatLng(latlng.lat,latlng.lng);
@@ -50,8 +52,10 @@ export class Attractions extends Component {
         };
         let counter = 0;
         let placesInfos = [];
-        console.log(request);
+        console.log('request',request);
         this.setState({testNum: 3});
+
+
 
 
     }
@@ -63,27 +67,21 @@ export class Attractions extends Component {
         //console.log('test if get location',this.props.city);
         return (
             <div>
-            <Tabs defaultActiveKey="1"  onChange={this.attractionSearch} className="attraction-tab">
-                <TabPane tab="food" key={TYPE_FOOD}>
-                    <Test num = {this.state.testNum}/>
+                <Tabs defaultActiveKey="1"  onChange={this.attractionSearch} className="attraction-tab">
+                    <TabPane tab="food" key={TYPE_FOOD}>
+                        <Test num = {this.state.testNum}/>
 
-                </TabPane>
+                    </TabPane>
 
-                <TabPane tab="shopping" key="2">
+                    <TabPane tab="shopping" key={TYPE_SHOPPING}>
 
-                </TabPane>
+                    </TabPane>
 
-                <TabPane tab="museum" key="3">
+                    <TabPane tab="museum" key={TYPE_MUSEUM}>
 
-                </TabPane>
-            </Tabs>
+                    </TabPane>
+                </Tabs>
             </div>
         );
     }
 }
-
-
-
-
-
-

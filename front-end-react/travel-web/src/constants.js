f9a7b9f55a5bf7dcf6d0150be541220e554f3637
Brokl <brokl0802@yahoo.com.tw>
2019-07-26 20:47:24 -0400

    Add one more city in city list

--- front-end-react/travel-web/src/constants.js
+++ front-end-react/travel-web/src/constants.js
@@ -26,28 +26,33 @@ export const MAX_DISPLAY = 6;
 export const DEFAULT_IMAGE= 'https://image.flaticon.com/icons/png/512/854/854878.png'
 export const CITY_LIST = [
     {
-     name: "London",
-     latlng: {lat: 51.506155, lng: -0.127824},
-     imageUrl: "https://images.unsplash.com/photo-1500380804539-4e1e8c1e7118?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+        name: "London",
+        latlng: {lat: 51.506155, lng: -0.127824},
+        imageUrl: "https://images.unsplash.com/photo-1500380804539-4e1e8c1e7118?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
     },
     {
-     name: "Paris",
-     latlng: {lat: 48.854429, lng: 2.352198},
-     imageUrl: "https://images.unsplash.com/photo-1485199433301-8b7102e86995?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1080&q=80"
+        name: "Paris",
+        latlng: {lat: 48.854429, lng: 2.352198},
+        imageUrl: "https://images.unsplash.com/photo-1485199433301-8b7102e86995?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1080&q=80"
     },
     {
-     name: "Milano",
-     latlng: {lat: 45.461719, lng: 9.190091},
-     imageUrl: "https://images.unsplash.com/photo-1512397739299-fe5a4327d192?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+        name: "Milano",
+        latlng: {lat: 45.461719, lng: 9.190091},
+        imageUrl: "https://images.unsplash.com/photo-1512397739299-fe5a4327d192?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
     },
     {
-     name: "New York",
-     latlng: {lat: 40.712921, lng: -74.006063},
-     imageUrl: "https://images.unsplash.com/photo-1496664232858-ae3480e5d308?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+        name: "New York",
+        latlng: {lat: 40.712921, lng: -74.006063},
+        imageUrl: "https://images.unsplash.com/photo-1496664232858-ae3480e5d308?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
     },
     {
         name: "Sydney",
         latlng: {lat: -33.8688197, lng: 151.2092955},
         imageUrl: "https://lonelyplanetimages.imgix.net/mastheads/65830387.jpg?sharp=10&vib=20&w=1200"
+    },
+    {
+        name: "Los Angeles",
+        latlng: {lat: 34.054671, lng: -118.250773},
+        imageUrl: "https://images.unsplash.com/photo-1493679756707-21f12da1a0c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1652&q=80"
     }
 ];

b37bfb69064220f5a34e25a5eb31157d91431352
Jieyu You <jyou61@gatech.edu>
2019-07-26 22:08:22 -0500

    x

--- front-end-react/travel-web/src/components/AttractionPost.js
+++ front-end-react/travel-web/src/components/AttractionPost.js
@@ -53,7 +53,9 @@ export class AttractionPost extends Component {
         // debugger
         const { name, icon, photos, geometry, place_id, rating, vicinity,types} = this.props.info;
         const curloc = geometry.location;
+        console.log(curloc)
         const location = {lat: curloc.lat? curloc.lat : curloc.lat() , lng: curloc.lng? curloc.lng : curloc.lng()};
+        console.log(location)
         // const description = `Rating ${rating}; Location: ${vicinity} `;
         const description =
             <ul className = 'description'>
